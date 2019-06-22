54e9e4195e1922f40a15a1f0d3e6a3c9d05dfb71
MRod21 <mario.rodriguez35@yahoo.com>
2019-06-22 18:45:38 -0500

    Deleted resource for Budget to remove optional row.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :budgets
   resources :transactions
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
