a85dba5b4119514ee7b19ec52ceb126215d34b83
MRod21 <mario.rodriguez35@yahoo.com>
2019-06-24 23:30:48 -0500

    Added email and first name validations.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     def create 
-        @user. = User.new(user_params)
+        @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
@@ -15,13 +15,13 @@ class UsersController < ApplicationController
     end
 
     def show 
-
+        @user = User.find_by_id(params[:id])
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:first_name, :email, :password)
     end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_many :transactions
-    has_many :budgets, through: :transactionsS
+    has_many :budgets, through: :transactions
+
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :first_name, presence: true
 
     has_secure_password
 end
