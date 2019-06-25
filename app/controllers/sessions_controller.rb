a3143c4c1d472d2ae793890f21219927c66ab38c
MRod21 <mario.rodriguez35@yahoo.com>
2019-06-25 16:02:37 -0500

    Defined create method for session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,5 +12,15 @@ class SessionsController < ApplicationController
 
     end
     
+    def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Sorry, login info was incorrect. Please try again."
+            redirect_to login_path
+        end
+    end
     
 end
